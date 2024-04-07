f82590494adeb05fc9d1c732ea0efcc8291b1ebe
Hieu Pham <hieupm2096@gmail.com>
2024-04-08 00:09:27 +0700

    feat: posts list, chore: migrate from nodejs to netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import node from "@astrojs/node";
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+  adapter: netlify(),
   prefetch: true,
   integrations: [tailwind()],
-  adapter: node({
-    mode: "standalone"
-  })
 });
